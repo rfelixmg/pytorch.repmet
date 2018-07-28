b65bb5c0aa022d5fdc6b4e8eb6c4981120e781fc
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2018-07-29 07:14:13 +0930

    cleanup and comments :P

--- run.py
+++ run.py
@@ -35,6 +35,16 @@ chunk_size = 32#128
 n_plot_samples = 15 # samples per class to plot
 n_plot_classes = 10
 
+# Define model and training parameters
+emb_dim = 2
+n_epochs = 15
+
+
+# Model
+net = MNISTEncoder(emb_dim)
+# net = resnet50(pretrained=False, num_classes=emb_dim)
+
+
 input_transforms = transforms.Compose([
     transforms.RandomResizedCrop(224, ratio=(1, 1.3333)),
     transforms.ToTensor()])
@@ -68,39 +78,18 @@ dataset = torchvision.datasets.MNIST(root='data/mnist/',
 test_dataset = torchvision.datasets.MNIST(root='data/mnist/',
                                           train=False,
                                           transform=transforms.ToTensor())
-#
-# # # Define training data
-# X = dataset.train_data
-# y = dataset.train_labels  # has 60,000 samples compared to tf version 55,000 this is due to val set?
 
-# X = X.numpy().astype(np.float32)
-# y = y.numpy().astype(np.uint8)
-
-# # Data loader
-# train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-#                                            batch_size=batch_size,
-#                                            shuffle=True)
-#
-# test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-#                                           batch_size=batch_size,
-#                                           shuffle=False)
-
-
-# def compute_reps(extract_fn, X, chunk_size):
-#     """Compute representations for input in chunks.
-#
-#     Basically just forwards the inputs through the net to get the embedding vectors in 'chunks'
-#     """
-#     chunks = int(ceil(float(X.shape[0]) / chunk_size))
-#     reps = []
-#     for i in range(chunks):
-#         start = i * chunk_size
-#         stop = start + chunk_size
-#         chunk_reps = extract_fn(torch.from_numpy(X[start:stop])).detach().cpu().numpy()
-#         reps.append(chunk_reps)
-#     return np.vstack(reps)
+n_iterations = int(ceil(float(len(dataset)) / batch_size))
+n_steps = n_iterations * n_epochs
+cluster_refresh_interval = n_iterations
 
 def get_inputs(dataset, indexs):
+    """
+    Gets the input data from a dataset
+    :param dataset: The dataset
+    :param indexs: List of the sample indexs
+    :return: A tensor with the inputs stacked
+    """
     inputs = None
     c = 0
     for index in indexs:
@@ -135,27 +124,8 @@ def compute_all_reps(net, dataset, chunk_size):
 
     Basically just forwards the inputs through the net to get the embedding vectors in 'chunks'
     """
-    # initial_reps = []
-    # for s in range(0, len(dataset), chunk_size):
-    #     indexs = list(range(s, min(s + chunk_size, len(dataset))))
-    #     initial_reps.append(compute_reps(net, dataset, indexs))
-    #
-    # return np.vstack(initial_reps)
-
     return compute_reps(net, dataset, list(range(len(dataset))), chunk_size=chunk_size)
 
-
-# Define model and training parameters
-emb_dim = 2
-n_epochs = 15
-n_iterations = int(ceil(float(len(dataset)) / batch_size))
-n_steps = n_iterations * n_epochs
-cluster_refresh_interval = n_iterations
-
-
-# Model
-net = MNISTEncoder(emb_dim)
-# net = resnet50(pretrained=False, num_classes=emb_dim)
 
 net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
 
@@ -164,23 +134,24 @@ net.cuda()
 cudnn.benchmark = True
 
 # Loss
-# criterion =
-optimizer = torch.optim.Adam(net.parameters(), lr=.00292)#1e-4)
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
 
 # Get initial embedding
 initial_reps = compute_all_reps(net, dataset, chunk_size)
 
+# Train labels from dataset (TODO make into a dataset variable / func)
 y = []
 for i in range(len(dataset)):
     y.append(dataset[i][1])
 y = np.asarray(y)
 
+# Test labels from dataset (TODO make into a dataset variable / func)
 test_labels = []
 for i in range(len(test_dataset)):
     test_labels.append(test_dataset[i][1])
 test_labels = np.asarray(test_labels)
 
-# Create batcher
+# Create batcher object (this also stores the cluster centroids than we move occasionally)
 batch_builder = ClusterBatchBuilder(y, k, m, d)
 batch_builder.update_clusters(initial_reps)
 
@@ -196,12 +167,14 @@ plot_test_classes = random.sample(set(test_labels), n_plot_classes)
 for pc in plot_test_classes:
     plot_test_sample_indexs += random.sample(set(np.arange(len(test_labels))[test_labels==pc]), n_plot_samples)
 
-# plot_embedding(compute_reps(net, dataset, plot_sample_indexs), y[plot_sample_indexs], savepath="%semb-e%d.pdf"%(plots_path,e+1))
+# lets plot the initial embeddings
 cluster_classes = batch_builder.cluster_classes
 
 #use this to get indexs (indx to match cluster classes) for class ids (plot_classes) that we are plotting
 for i in range(len(cluster_classes)):
     cluster_classes[i] = batch_builder.unique_classes[cluster_classes[i]]
+
+# plot it
 graph(initial_reps[plot_sample_indexs], y[plot_sample_indexs],
       cluster_centers=batch_builder.centroids,
       cluster_classes=batch_builder.cluster_classes,
@@ -213,6 +186,7 @@ graph(initial_reps[plot_sample_indexs], y[plot_sample_indexs],
 # print("Testing set stats:")
 # test_dataset.stats()
 
+# Lets setup the training loop
 batch_losses = []
 batch_accs = []
 test_accs = []
@@ -223,24 +197,28 @@ for e in range(n_epochs):
     for i in range(n_iterations):
         # Sample batch and do forward-backward
         batch_example_inds, batch_class_inds = batch_builder.gen_batch()
-        # inputs = dataset.train_data[batch_example_inds].float().cuda()
 
+        # Get inputs and and labels from the dataset
         inputs = get_inputs(dataset, batch_example_inds).cuda()
         labels = torch.from_numpy(batch_class_inds).cuda()
 
+        # Calc the outputs (embs) and then the loss + accs
         outputs = net(inputs)
         batch_loss, batch_example_losses, batch_acc = minibatch_magnet_loss(outputs, labels, m, d, alpha)
 
+        # pass the gradient and update
         optimizer.zero_grad()
         batch_loss.backward()
         optimizer.step()
 
+        # just changing some types
         batch_loss = float(batch_loss.detach().cpu().numpy())
         batch_example_losses = batch_example_losses.detach().cpu().numpy()
 
         # Update loss index
         batch_builder.update_losses(batch_example_inds, batch_example_losses)
 
+        # store the stats to graph at end
         batch_losses.append(batch_loss)
         batch_accs.append(batch_acc)
         test_accs.append(test_acc)
@@ -255,7 +233,6 @@ for e in range(n_epochs):
     reps = compute_all_reps(net, dataset, chunk_size)
     batch_builder.update_clusters(reps)
 
-    # plot_embedding(compute_reps(net, dataset, plot_sample_indexs), y[plot_sample_indexs], savepath="%semb-e%d.pdf"%(plots_path,e+1))
     cluster_classes = batch_builder.cluster_classes
 
     #use this to get indexs (indx to match cluster classes) for class ids (plot_classes) that we are plotting
