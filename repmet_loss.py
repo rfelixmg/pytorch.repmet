60892b353b6d17d80164b3a7f8ecd55e46747957
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2018-08-27 06:50:16 +0930

    clean up and double check of the repmet losses

--- repmet_loss.py
+++ repmet_loss.py
@@ -43,31 +43,29 @@ class RepMetLoss(Loss):
         """
 
         # Compute distance of each example to each cluster centroid (euclid without the root)
-        sample_costs = self.calculate_distance(self.centroids, x)
+        distances = self.calculate_distance(self.centroids, x)
 
         # Compute the two masks selecting the sample_costs related to each class(r)=class(cluster/rep)
         intra_cluster_mask = self.comparison_mask(y, torch.from_numpy(self.cluster_classes).cuda())
         intra_cluster_mask_op = ~intra_cluster_mask
 
-        # Calculate the minimum distance to rep of different class
-        # therefore we make all values for correct clusters bigger so they are ignored..
-        intra_cluster_costs_ignore_op = (intra_cluster_mask.float() * (sample_costs.max() * 1.5))
-        intra_cluster_costs_desired_op = intra_cluster_mask_op.float() * sample_costs
-        intra_cluster_costs_together_op = intra_cluster_costs_ignore_op + intra_cluster_costs_desired_op
-        min_non_match, _ = intra_cluster_costs_together_op.min(1)
-
         # Calculate the minimum distance to rep of same class
         # therefore we make all values for other clusters bigger so they are ignored..
-        intra_cluster_costs_ignore = (intra_cluster_mask_op.float() * (sample_costs.max() * 1.5))
-        intra_cluster_costs_desired = intra_cluster_mask.float() * sample_costs
+        intra_cluster_costs_ignore = (intra_cluster_mask_op.float() * (distances.max() * 1.5))
+        intra_cluster_costs_desired = intra_cluster_mask.float() * distances
         intra_cluster_costs_together = intra_cluster_costs_ignore + intra_cluster_costs_desired
         min_match, _ = intra_cluster_costs_together.min(1)
 
+        d = ensure_numpy(distances)
+        t = ensure_numpy(intra_cluster_costs_ignore)
+        tt = ensure_numpy(intra_cluster_costs_desired)
+        ttt = ensure_numpy(intra_cluster_costs_together)
+        tttt = ensure_numpy(min_match)
         # Compute variance of intra-cluster distances
         # N = x.shape[0]
         # variance = min_match.sum() / float((N - 1))
-        variance = 0.5  # hard code 0.5 [as suggested in paper] but seems to now work as well as the calculated variance in my exp
-        var_normalizer = -1 / (2 * variance)
+        variance = 0.5  # hard code 0.5 [as suggested in paper]
+        var_normalizer = -1 / (2 * variance**2)
 
         if not self.avg_variance:
             self.avg_variance = variance
@@ -78,29 +76,26 @@ class RepMetLoss(Loss):
         numerator = torch.exp(var_normalizer * min_match)
 
         # Compute denominator
-        # diff_class_mask = tf.to_float(tf.logical_not(comparison_mask(classes, cluster_classes)))
         diff_class_mask = intra_cluster_mask_op.float()
-        denom_sample_costs = torch.exp(var_normalizer * sample_costs)
+        denom_sample_costs = torch.exp(var_normalizer * distances)
         denominator = (diff_class_mask.cuda() * denom_sample_costs).sum(1)
 
         # Compute example losses and total loss
         epsilon = 1e-8
 
         # Compute example losses and total loss
-        losses_eq5 = F.relu(-torch.log(numerator / (denominator + epsilon) + epsilon) + self.alpha)
+        losses = F.relu(-torch.log(numerator / (denominator + epsilon) + epsilon) + self.alpha)
 
-        losses = losses_eq5
         total_loss = losses.mean()
 
-        _, preds = sample_costs.min(1)
+        _, preds = distances.min(1)
         preds = ensure_tensor(self.cluster_classes[preds]).cuda()  # convert from cluster ids to class ids
         acc = torch.eq(y, preds).float().mean()
 
         return total_loss, losses, acc
 
 
-
-# TODO: implement version 2 where they don't only take min of class in numerator
+# Don't only take min of class in numerator and use all in denominator
 class RepMetLoss2(Loss):
 
     def __init__(self, *args, **kwargs):
@@ -137,16 +132,16 @@ class RepMetLoss2(Loss):
         """
 
         # Compute distance of each example to each cluster centroid (euclid without the root)
-        sample_costs = self.calculate_distance(self.centroids, x)
+        distances = self.calculate_distance(self.centroids, x)
 
-        # Compute the two masks selecting the sample_costs related to each class(r)=class(cluster/rep)
+        # Compute the mask selecting the distances related to each class(r)=class(cluster/rep)
         intra_cluster_mask = self.comparison_mask(y, torch.from_numpy(self.cluster_classes).cuda())
 
         # Compute variance of intra-cluster distances
         # N = x.shape[0]
         # variance = min_match.sum() / float((N - 1))
-        variance = 0.5  # hard code 0.5 [as suggested in paper] but seems to now work as well as the calculated variance in my exp
-        var_normalizer = -1 / (2 * variance)
+        variance = 0.5  # hard code 0.5 [as suggested in paper]
+        var_normalizer = -1 / (2 * variance**2)
 
         if not self.avg_variance:
             self.avg_variance = variance
@@ -154,11 +149,12 @@ class RepMetLoss2(Loss):
             self.avg_variance = (self.avg_variance + variance) / 2
 
         # Compute numerator
-        sample_costs_e = torch.exp(var_normalizer * sample_costs)
-        numerator = (intra_cluster_mask.float() * sample_costs_e).sum(1)
+        numerator_pre_mask = torch.exp(var_normalizer * distances)
+        numerator = (intra_cluster_mask.float() * numerator_pre_mask).sum(1)
 
         # Compute denominator
-        denominator = sample_costs_e.sum(1)
+        denominator = numerator_pre_mask.sum(1)
+
         # Compute example losses and total loss
         epsilon = 1e-8
 
@@ -167,16 +163,14 @@ class RepMetLoss2(Loss):
 
         total_loss = losses.mean()
 
-        _, preds = sample_costs.min(1)
+        _, preds = distances.min(1)
         preds = ensure_tensor(self.cluster_classes[preds]).cuda()  # convert from cluster ids to class ids
         acc = torch.eq(y, preds).float().mean()
 
         return total_loss, losses, acc
 
 
-
-
-# TODO: implement version 3 where they don't only take min of class in numerator, still exclude from denominator though
+# Don't only take min of class in numerator, still exclude from denominator though
 class RepMetLoss3(Loss):
 
     def __init__(self, *args, **kwargs):
@@ -222,7 +216,7 @@ class RepMetLoss3(Loss):
         # N = x.shape[0]
         # variance = min_match.sum() / float((N - 1))
         variance = 0.5  # hard code 0.5 [as suggested in paper] but seems to now work as well as the calculated variance in my exp
-        var_normalizer = -1 / (2 * variance)
+        var_normalizer = -1 / (2 * variance**2)
 
         if not self.avg_variance:
             self.avg_variance = variance
@@ -239,7 +233,7 @@ class RepMetLoss3(Loss):
         epsilon = 1e-8
 
         # Compute example losses and total loss
-        losses = F.relu(-torch.log(numerator / (denominator + epsilon) + epsilon))
+        losses = F.relu(-torch.log(numerator / (denominator - numerator + epsilon) + epsilon))
 
         total_loss = losses.mean()
 
