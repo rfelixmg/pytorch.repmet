magnet loss debug, fixed denom masking based on clusters rather than cluster classes

magnet_loss.py
@@ -1,17 +1,16 @@
-import numpy as np
-from sklearn.cluster import KMeans
 import torch
 import torch.nn.functional as F
 
 from loss import Loss
-from utils import ensure_tensor
+
 
 class MagnetLoss(Loss):
-    """Sample minibatches for magnet loss."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def loss(self, x, y):  # y isn't used as set out of batch is just based on means of x as the y's
+    def loss(self, x, y):
+        # y is used to specify the sum constraint where classes different,
+        # for the case where k>1 and multiple imposter classes are from the same class
         """Compute magnet loss.
 
         Given a tensor of features `x`, the assigned class for each example,
@@ -34,18 +33,21 @@ class MagnetLoss(Loss):
         cluster_examples = x.view(self.m, int(float(x.shape[0]) / float(self.m)), x.shape[1])
         cluster_means = cluster_examples.mean(1)
 
+        cluster_examples_y = y.view(self.m, int(float(y.shape[0]) / float(self.m)))
+        cluster_ys, _ = cluster_examples_y.max(1)
+
         # Compute squared distance of each example to each cluster centroid (euclid without the root)
-        sample_costs = self.calculate_distance(cluster_means, x)
+        distances = self.calculate_distance(cluster_means, x)
 
         # Select distances of examples to their own centroid
         intra_cluster_mask = self.comparison_mask(batch_inds, torch.arange(self.m).cuda())
-        diff_class_mask = ~intra_cluster_mask
-        intra_cluster_costs = torch.sum(intra_cluster_mask.float() * sample_costs, dim=1)
+        diff_class_mask = ~self.comparison_mask(y, cluster_ys)
+        intra_cluster_costs = torch.sum(intra_cluster_mask.float() * distances, dim=1)
 
         # Compute variance of intra-cluster distances
         N = x.shape[0]
         variance = intra_cluster_costs.sum() / float((N - 1))
-        var_normalizer = -1 / (2 * variance)
+        var_normalizer = -1 / (2 * variance**2)  # unsure whether this should be squared, is in eq(5) but not in eq(4)
 
         if not self.avg_variance:
             self.avg_variance = variance
@@ -56,7 +58,7 @@ class MagnetLoss(Loss):
         numerator = torch.exp(var_normalizer * intra_cluster_costs - self.alpha)
 
         # Compute denominator
-        denom_sample_costs = torch.exp(var_normalizer * sample_costs)
+        denom_sample_costs = torch.exp(var_normalizer * distances)
         denominator = torch.sum(diff_class_mask.float() * denom_sample_costs, dim=1)
 
         # Compute example losses and total loss
@@ -64,7 +66,7 @@ class MagnetLoss(Loss):
         losses = F.relu(-torch.log(numerator / (denominator + epsilon) + epsilon))
         total_loss = losses.mean()
 
-        _, pred_inds = sample_costs.min(1)
+        _, pred_inds = distances.min(1)
         acc = torch.eq(batch_inds, pred_inds.float()).float().mean()
 
         return total_loss, losses, acc